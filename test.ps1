2728392feeb6b83f3974ff1e81cfa699c1247423
ReverseTn <39220011+medali047@users.noreply.github.com>
2023-01-18 00:59:22 +0100

    Delete test.ps1

--- test.ps1
+++
@@ -1,5 +0,0 @@
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls,Tls11,Tls12'
-
-Invoke-WebRequest -Uri 'https://github.com/medali047/test/raw/main/putty.exe' -OutFile Test-Me-64bit.exe
-
-.\Test-Me-64bit.exe
